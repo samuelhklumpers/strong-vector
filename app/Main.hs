3dc1d0acf7f58ca0b1b0c94c8bd7a7f67970ebf3
Samuel Klumpers <samuelhklumpers@gmail.com>
2022-02-16 11:15:16 +0100

    re-encode Fin, eliminating Known

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,6 @@
 
 module Main where
 
-import Data.Constraint ( (\\), unmapDict, type (:-), Dict(..) )
 import Data.List ( intercalate )
 
 
@@ -13,7 +12,8 @@ data Nat n where
     NS :: Nat n -> Nat ('S n)
 
 data Fin n where
-    FZ :: Fin ('S n)
+    FI :: Fin ('S 'Z)
+    FZ :: Fin ('S n) -> Fin ('S ('S n))
     FS :: Fin ('S n) -> Fin ('S ('S n))
 
 data Vec a n where -- the order of the params makes Vec not a Functor oops
@@ -43,17 +43,18 @@ toInt (NS n) = 1 + toInt n
 instance Show (Nat n) where
     show n = "Nat " ++ show (toInt n)
 
-fromFin :: forall n. Known ('S n) => Fin ('S n) -> Nat ('S n)
-fromFin FZ     = nat
-fromFin (FS _) = nat \\ (Dict :: Dict (Known ('S ('S n))))
--- ^ why does this not need a @down@?????
+fromFin :: Fin ('S n) -> Nat ('S n)
+fromFin FI     = NS NZ
+fromFin (FZ f) = NS (fromFin f)
+fromFin (FS f) = NS (fromFin f)
 
-finToTup :: forall n. Known ('S n) => Fin ('S n) -> (Int, Int)
-finToTup f@FZ  = (0, toInt $ fromFin f)
+finToTup :: Fin ('S n) -> (Int, Int)
+finToTup FI     = (0, 1)
+finToTup (FZ f) = (0, 1 + toInt (fromFin f))
 finToTup (FS f) = (x + 1, y + 1) where
-    (x, y) = finToTup f \\ (down Dict :: Dict (Known n))
+    (x, y) = finToTup f
 
-instance Known ('S n) => Show (Fin ('S n)) where
+instance Show (Fin ('S n)) where
     show f = "Fin " ++ show x ++ "/" ++ show y where
         (x, y) = finToTup f
 
@@ -77,66 +78,43 @@ vConc :: Vec a n -> Vec a m -> Vec a (n + m)
 vConc VN w     = w
 vConc (VC x v) w = VC x (vConc v w)
 
-vLen :: Known n => Vec a n -> Nat n
+vLen :: Vec a n -> Nat n
 vLen VN = NZ
-vLen (VC _ _) = nat
+vLen (VC _ v) = NS (vLen v)
+
+toFin :: Nat n -> Fin ('S n)
+toFin NZ     = FI
+toFin (NS n) = FZ $ toFin n
 
 enumF :: Nat n -> Vec (Fin n) n
-enumF NZ     = VN
-enumF (NS NZ) = VC FZ VN
-enumF (NS (NS n)) = VC FZ (vMap FS (enumF (NS n)))
+enumF NZ          = VN
+enumF (NS NZ)     = VC FI VN
+enumF (NS (NS n)) = VC (toFin $ NS n) (vMap FS (enumF (NS n)))
 
 vSum :: Num a => Vec a n -> a
 vSum = vFold (+) 0
 
 delete :: Fin ('S n) -> Vec a ('S n) -> Vec a n
-delete FZ (VC  _ v)        = v
-delete (FS  FZ) (VC _ v) = v
-delete (FS  (FS f)) (VC x v) = VC x (delete (FS f) v)
+delete FI (VC _ VN)    = VN
+delete (FZ _) (VC _ v) = v
+delete (FS f) (VC x v) = VC x $ delete f v
 
 subMatrix :: Fin ('S n) -> Fin ('S m) -> Vec (Vec a ('S m)) ('S n) -> Vec (Vec a m) n
 subMatrix f g v = vMap (delete g) $ delete f v
 
--- | not quite the determinant, put (-1) ** x here when implemented
-det :: (Known n, Num a) => Vec (Vec a n) n -> a
+det :: Num a => Vec (Vec a n) n -> a
 det VN             = 0
 det (VC (VC x _) VN) = x
-det v@VC {} = vSum $ vZip f (enumF $ vLen v) v where
-    f j (VC x _) = (if even' j then 1 else -1) * x * minor FZ j v
+det v@(VC _ w) = vSum $ vZip f (enumF $ vLen v) v where
+    f j (VC x _) = (if even' j then 1 else -1) * x * minor (toFin $ vLen w) j v
 
     even' :: Fin n -> Bool
-    even' FZ = True
-    even' (FS n) = not $ even' n
+    even' FI     = True
+    even' (FZ g) = even' g
+    even' (FS g) = not $ even' g
 
-minor :: forall n a. (Known ('S n), Num a) => Fin ('S n) -> Fin ('S n) -> Vec (Vec a ('S n)) ('S n) -> a
-minor i j v = det (subMatrix i j v) \\ (down Dict :: Dict (Known n))
+minor :: Num a => Fin ('S n) -> Fin ('S n) -> Vec (Vec a ('S n)) ('S n) -> a
+minor i j v = det (subMatrix i j v)
 
 main :: IO ()
 main = putStrLn "hoi"
-
-
-class Known n where
-    nat :: Nat n
-
-instance Known 'Z where
-    nat = NZ
-
-instance Known n => Known ('S n) where
-    nat = NS nat
-
-know :: Nat n -> Dict (Known n)
-know NZ = Dict
-know (NS n) = case know n of
-    Dict -> Dict
-
-down :: Dict (Known ('S n)) -> Dict (Known n)
-down Dict = h nat where
-    h :: Nat ('S n) -> Dict (Known n)
-    h (NS n) = know n
-
-down' :: Known ('S n) :- Known n -- turns out this one is less useful than the one above
-down' = unmapDict down              -- or is it?
-
-test :: forall n. Known ('S n) => Nat n
-test = case down Dict of
-    (Dict :: Dict (Known n)) -> nat
