fbeebb753925753a2b68b9951a532b6afd81b719
Samuel Klumpers <samuelhklumpers@gmail.com>
2022-03-01 22:55:42 +0100

    doc

--- src/Naturals.hs
+++ src/Naturals.hs
@@ -1,83 +1,126 @@
 {-# LANGUAGE DataKinds, TypeFamilies, GADTs, TypeApplications, ConstraintKinds, ScopedTypeVariables, FlexibleInstances, TypeOperators, FlexibleContexts, MultiParamTypeClasses, EmptyCase, AllowAmbiguousTypes #-}
 
+-- | This module declares the type-level naturals for the type signatures of sized vectors,
+-- along with the necessary machinery to manipulate them.
 module Naturals where
 
 import Data.Bifunctor (bimap)
 import Data.Constraint ( Dict(..), (\\) )
 import Data.Void (Void)
 
-
+-- | The natural numbers type.
 data N = Z | S N
+-- Note that -XDataKinds lifts @N@ to a kind, and @Z@ and @S@ to type constructors
 
+-- | The singleton type for natural numbers.
 data Nat n where
     NZ :: Nat 'Z
     NS :: Nat n -> Nat ('S n)
+-- The "singleton" in "singleton type" stems from the fact that for each choice of type parameters, the type has only a single inhabitant,
+-- i.e. is a singleton in the terminology of sets.
 
+-- | The finite set type.
+-- Remark that @Fin n@ has @n@ values.
 data Fin n where
+    -- we use ('S n) here rather than n because:
+    -- 1. Fin 'Z is uninhabited eitherway
+    -- 2. some functions expect 'S n, for example see @delete@
     FZ :: Fin ('S n)
     FS :: Fin ('S n) -> Fin ('S ('S n))
 
+-- | Proof that @f :: Fin 'Z@ if and only if @f@ is @undefined@
 fin0 :: Fin 'Z -> Void
-fin0 f = case f of {}
+fin0 f = case f of {} -- there is no constructor for @Fin 'Z@, so we can discharge @f@ by simply pattern matching into an empty case expression
 
+-- | Alternative to @FS@ that does not require @n ~ 'S m@
 finS :: KnownNat n => Fin n -> Fin ('S n)
 finS (f :: Fin n) = case nat :: Nat n of
     NZ   -> case f of {}
     NS _ -> FS f
 
--- no generalized injectivity annotations :(
+-- | Type-level addition
 type family (n :: N) + (m :: N) :: N where
+    -- no generalized injectivity annotations :(
+    -- => concretely this means we have to disambiguate every function that takes some @n + m@ containing argument
     'Z + m     = m
     ('S n) + m = 'S (n + m)
 
 instance Show (Nat n) where
     show n = "Nat " ++ show (toInt n)
 
-instance KnownNat n => Show (Fin ('S n)) where
+instance KnownNat n => Show (Fin n) where
     show f = "Fin " ++ show x ++ "/" ++ show y where
         (x, y) = finToTup f
 
+-- | Convert @s :: Nat n@ to @n :: Int@
 toInt :: Nat n -> Int
 toInt NZ = 0
 toInt (NS n) = 1 + toInt n
 
+-- | Natural singleton addition
 (+|) :: Nat n -> Nat m -> Nat (n + m)
-NZ +| n     = n
+NZ     +| n = n -- the definition of the @+@ family makes everything here typecheck smoothly
 (NS n) +| m = NS (n +| m)
 
+-- | Return the size of the set this @Fin@ is from
 finSize :: KnownNat n => Fin n -> Nat n
 finSize _ = nat
 
+{- |
+    Return the index and size of a @Fin@. 
+
+    For example, @FS (FS FZ) :: Fin ('S ('S ('S ('S 'Z))))@ would become @(1, 4)@, indicating that it points to the 2nd element of a set of 4 elements. -}
 finToTup :: KnownNat n => Fin n -> (Int, Int)
 finToTup f@FZ = (0, toInt $ finSize f)
 finToTup ((FS f) :: Fin n) = bimap (+1) (+1) $ finToTup f \\ lower (Dict @(KnownNat n))
 
+-- | Point to the 0th (lowest) index in @Fin ('S n)@
 toFZ :: Nat n -> Fin ('S n)
 toFZ _ = FZ
 
+-- | Point to the @n@th (highest) index in @Fin ('S n)@
 toFS :: Nat n -> Fin ('S n)
 toFS NZ     = FZ
 toFS (NS n) = FS $ toFS n
 
-
--- Given doesn't work well: it makes instance constraints as large as instance heads, making some (all) undecidable
+-- | This class associates singleton values to type-level naturals.
+-- Note that, unintuitively, @KnownNat n@ not hold universally for @n :: N@, (barring type reflection).
+-- However, @KnownNat@ will hold for any concrete type, which is why we (I) refer to those @n@ with @KnownNat n@ as "constructible".
 class KnownNat n where
+    -- but also note that type reflection would not help either
+    -- as in that case we again have to carry around @Typeable@
+
+    -- Given doesn't work well: it makes instance constraints as large as instance heads, making some (all) undecidable
     nat :: Nat n
 
+-- these instances ensure that any concrete @n@ has @KnownNat n@
 instance KnownNat 'Z where
     nat = NZ
 
 instance KnownNat n => KnownNat ('S n) where
     nat = NS nat
 
+-- | If we have a singleton @Nat n@, then @n@ is constructible
 know :: Nat n -> Dict (KnownNat n)
-know NZ = Dict
-know (NS n) = Dict \\ know n
+know NZ     = Dict              -- KnownNat 'Z
+know (NS n) = Dict \\ know n    -- Apply KnownNat n => KnownNat ('S n) to KnownNat n
 
+-- | If @'S n@ is constructible, then @n@ is constructible
 lower :: Dict (KnownNat ('S n)) -> Dict (KnownNat n)
 lower Dict = h nat where
+    -- first construct @s :: Nat ('S n)@,
+    -- then destruct it to get @t :: Nat n@,
+    -- which is sufficient to prove @KnownNat n@.
     h :: Nat ('S n) -> Dict (KnownNat n)
     h (NS n) = know n
+-- we can use lower as follows
+-- x \\ lower (Dict @(KnownNat n))
+-- to promote some x :: KnownNat m => a with @n ~ 'S m@
+-- to KnownNat n => a
+
+-- note that we use type applications here, like @(KnownNat n)
+-- in this case, this is syntactic sugar over writing @Dict :: Dict (KnownNat n)@
+
 
 {-
 lower' :: KnownNat ('S n) :- KnownNat n
--- src/Vectors.hs
+++ src/Vectors.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DataKinds, TypeFamilies, GADTs, ScopedTypeVariables, FlexibleInstances, TypeOperators, FlexibleContexts, TypeApplications, AllowAmbiguousTypes #-}
 
+-- | This module defines a fixed-size vector datatype,
+-- and includes the instances and tools to allow for user-friendly manipulation.
 module Vectors where
 
 import Control.Applicative ( liftA2 )
@@ -17,32 +19,41 @@ import qualified Prelude as P
 import Naturals ( type (+), Fin(..), Nat(..), N(S, Z), KnownNat (nat), lower, toFZ, finS, know, toInt )
 
 
-data L = Nil | Cons N L -- restricted to N for now
--- refer to SingKind for generalized stuff.
-
-data List xs where
-    LN :: List 'Nil
-    LC :: Nat n -> List xs -> List ('Cons n xs)
-
+-- | The singleton type for vectors
 data Vec n a where
     VN :: Vec 'Z a
     VC ::  a -> Vec n a -> Vec ('S n) a
 
+-- | The list of natural numbers type
+data L = Nil | Cons N L
+-- restricted to N for now
+-- refer to SingKind for generalized stuff.
+
+-- | The singleton type for lists of natural numbers
+data List xs where
+    LN :: List 'Nil
+    LC :: Nat n -> List xs -> List ('Cons n xs)
+
+{-
 data Tensor ix a where
     TN :: Tensor 'Nil a
     TC :: Vec n (Tensor ix a) -> Tensor ('Cons n ix) a
+-}
+
 
 instance Show a => Show (Vec n a) where
     show v = "<" P.++ intercalate "," (map show $ toList v) P.++ ">"
 
 instance Eq a => Eq (Vec n a) where
-    VN == VN         = True
+    VN     == VN     = True
     VC x v == VC y w = x == y && v == w
 
 instance Functor (Vec n) where
     fmap _ VN       = VN
     fmap f (VC x v) = VC (f x) (fmap f v)
 
+-- | Note that this instance is forced to use @full@ for @pure@,
+-- unlike unsized vectors of lists which use the singleton @pure x = [x]@.
 instance KnownNat n => Applicative (Vec n) where
     pure x = full x nat
 
@@ -58,17 +69,20 @@ instance Foldable (Vec n) where
   foldMap f (VC x v) = f x <> foldMap f v
 
 instance KnownNat n => Traversable (Vec n) where
-  sequenceA VN = pure VN
+  sequenceA VN       = pure VN
   sequenceA (VC x v) = VC <$> x <*> (sequenceA v \\ lower (Dict @(KnownNat n)))
 
+-- | Note that only non-empty vectors are comonads due to @extract@
 instance Comonad (Vec ('S n)) where
     extract (VC x _) = x
-    duplicate v = fmap (const v) v
+    duplicate = square
 
 instance KnownNat n => Distributive (Vec n) where
-    distribute = distributeRep -- this gives us nice(r) transposes I think
+    distribute = distributeRep
+    -- this gives us nice(r) transposes I think
     -- or at least we can now transpose through [] or so
 
+-- | A vector of size @n@ with elements of type @a@ is isomorphic to a function from a type with @n@ values into @a@.
 instance KnownNat n => Representable (Vec n) where
     type Rep (Vec n) = Fin n
 
@@ -85,95 +99,115 @@ instance (KnownNat n, Num a) => Num (Vec n a) where
 
     fromInteger = pure . fromInteger
 
-
+-- | Safely index a vector with an appropriate index
 get :: Vec n a -> Fin n -> a
 get (VC a _) FZ     = a
 get (VC _ v) (FS f) = get v f
 
-
+-- | Generate a vector by applying a function to a range of indices
 generateN :: Nat n -> (Fin n -> a) -> Vec n a
 generateN NZ _     = VN
 generateN (NS n) f = VC (f FZ) (generateN n (f . finS \\ know n))
 
+-- | Generate a vector by applying a function to a range of indices, inferring the size from the type
 generate :: KnownNat n => (Fin n -> a) -> Vec n a
 generate = generateN nat
 
-
+-- | Build a vector by repeatedly applying a function to a seed
 unfoldN :: Nat n -> (s -> (a, s)) -> s -> Vec n a
 unfoldN NZ _ _ = VN
 unfoldN (NS n) f z = VC a (unfoldN n f s) where
     (a, s) = f z
 
+-- | Build a vector by repeatedly applying a function to a seed, inferring the size from the type
 unfold :: KnownNat n => (s -> (a, s)) -> s -> Vec n a
 unfold = unfoldN nat
 
-
+-- | @iterateN n f x@ returns a vector of size @n@ of repeated applications of @f@ to @x@
 iterateN :: Nat n -> (a -> a) -> a -> Vec n a
 iterateN NZ     _ _ = VN
 iterateN (NS n) f z = VC z (iterateN n f (f z))
 
+-- | @iterate f x@ returns a vector of repeated applications of @f@ to @x@, inferring the size from the type
 iterate :: KnownNat n => (a -> a) -> a -> Vec n a
 iterate = iterateN nat
 
-
+-- | @linspace x y n@ returns a vector of @n@ points uniformly spaced in the interval @[x, y)@.
 linspace :: Fractional a => a -> a -> Nat n -> Vec n a
 linspace x y n = iterateN n step x where
     step z = z + (y - x) / fromIntegral (toInt n)
 
-
+-- | @take n v@ returns a prefix of size @n@ from @v@
 take :: forall m n a. Nat n -> Vec (n + m) a -> Vec n a
 take NZ     _                   = VN
-take (NS (n :: Nat k)) (VC a v) = VC a $ take @m n v
+take (NS (n :: Nat k)) (VC a v) = VC a $ take @m n v -- we need to use take @m here to disambiguate m, since it is ambiguous because GHC does not recognize injectivity of n + m over m
 
+-- | @drop n v@ returns the suffix after @n@ elements of @v@
 drop :: forall n m a. Nat n -> Vec (n + m) a -> Vec m a
 drop NZ v = v
 drop (NS k) (VC _ v) = drop k v
 
-
+-- | Safely return the first element of a non-empty vector
 head :: Vec ('S n) a -> a
 head (VC a _) = a
 
+-- | Safely return the tail of a non-empty vector
 tail :: Vec ('S n) a -> Vec n a
 tail (VC _ v) = v
 
-
+-- | Zip two equally long vectors with a binary function
 zipWith :: (a -> b -> c) -> Vec n a -> Vec n b -> Vec n c
 zipWith _ VN       VN = VN
 zipWith f (VC a v) (VC b w) = VC (f a b) (zipWith f v w)
 
+-- | Return the diagonal of a square matrix
 diag :: Vec n (Vec n a) -> Vec n a
 diag v = zipWith get v (enumFin $ size v)
 
+-- | Square a functorial value.
+-- For container-like functors, this tends to actually make square containers.
+square :: Functor f => f b -> f (f b)
+square v = fmap (const v) v
+
+-- | Convert a vector to a list
 toList :: Vec n a -> [a]
 toList = foldl (flip (:)) []
 
+-- | @full x n@ returns a vector of @n@ copies of @x@
 full :: a -> Nat n -> Vec n a
 full _ NZ     = VN
 full a (NS n) = VC a (full a n)
 
+-- | Concatenate two vectors
 (++) :: Vec n a -> Vec m a -> Vec (n + m) a
-VN ++ w     = w
+VN     ++ w = w
 VC x v ++ w = VC x (v ++ w)
 
+-- | Return the number of elements of a vector
 size :: Vec n a -> Nat n
 size VN = NZ
 size (VC _ v) = NS (size v)
 
+-- | @enumFin n@ returns a vector of size @n@, enumerating from @0@ to @n@ in @Fin n@
 enumFin :: Nat n -> Vec n (Fin n)
 enumFin NZ          = VN
 enumFin (NS NZ)     = VC FZ VN
 enumFin (NS (NS n)) = VC (toFZ $ NS n) (fmap FS (enumFin (NS n)))
 
+-- | Tuple the elements of a vector with their indices
 enumerate :: Vec n a -> Vec n (Fin n, a)
 enumerate v = zipWith (,) (enumFin $ size v) v
 
+-- | @delete n v@ safely returns @v@ with the @n@th element deleted, provided @v@ is non-empty
 delete :: Fin ('S n) -> Vec ('S n) a -> Vec n a
 delete FZ (VC _ v)    = v
 delete (FS f) (VC x v) = VC x $ delete f v
 
+-- | @subMatrix n m x@ returns the submatrix obtained by deleting the @n@th row and @m@th column, provided @x@ is non-empty
 subMatrix :: Fin ('S n) -> Fin ('S m) -> Vec ('S n) (Vec ('S m) a) -> Vec n (Vec m a)
 subMatrix f g v = delete g <$> delete f v
 
+-- | Return the determinant of a square matrix
 det :: forall a n. Num a => Vec n (Vec n a) -> a
 det VN               = 0
 det (VC (VC x _) VN) = x
@@ -186,6 +220,7 @@ det v@(VC _ w)       = sum $ cof <$> enumerate v
     sign (FS FZ) = -1
     sign (FS (FS f)) = sign f
 
+-- | Return the minor of a non-empty square matrix
 minor :: Num a => Fin ('S n) -> Fin ('S n) -> Vec ('S n) (Vec ('S n) a) -> a
 minor i j v = det (subMatrix i j v)
 
