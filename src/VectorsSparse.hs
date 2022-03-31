415b974f8a6192aa785f327dcfc4343162689d68
Richard Koetschruyter <richardkoetschruyter@gmail.com>
2022-03-31 18:28:34 +0200

    toSparse works.

--- src/VectorsSparse.hs
+++ src/VectorsSparse.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module VectorsSparse where
 
 import Data.Map ( empty, fromList, lookup, toDescList, Map, toAscList, insert )
@@ -18,14 +20,18 @@ exampleSparse :: SVec 'Z Int
 exampleSparse = (3, empty)
 
 exampleSparse2 :: SVec N4 Int
-exampleSparse2 = (2, fromList [(FS FZ, 5), (FS $ FS FZ, 6)])
+exampleSparse2 = (2, fromList [(FS FZ, 5), (FS $ FS FZ, 5)])
+
+
+exampleVec3 :: Vec N4 Int
+exampleVec3 = VC 2 $ VC 5 $ VC 5 $ VC 2 VN
 
 -- | Return the value at index n if it exists. Otherwise return the default sparse value.
 getS :: SVec n a -> Fin n -> a
 getS (sV, m) n = fromMaybe sV $ Data.Map.lookup n m
 
 -- | Insert (overwrite) a value at an index if it is not the default value.
-putS :: SVec ('S n) a -> Fin ('S n) -> a -> SVec ('S n) a
+putS :: Eq a => SVec n a -> Fin n -> a -> SVec n a
 putS (sV, m) n v | sV == v   = (sV, m)
                  | otherwise = (sV, insert n v m)
 
@@ -33,12 +39,11 @@ putS (sV, m) n v | sV == v   = (sV, m)
 fromSparse :: KnownNat n => SVec n a -> Vec n a
 fromSparse sVec = generateN nat $ getS sVec
 
--- | Given a vector and sparse value, produce a sparse vector
-toSparse :: Eq a => Vec n a -> a -> SVec ('S n) a
-toSparse v sV = go v sV FZ
-       where go :: Eq a => Vec n a -> a -> Fin ('S n) -> SVec ('S n) a
-             go VN sV i                   = (sV, empty)
-             go (VC x v') sV i | x == sV   = svec
-                               | otherwise = putS svec i x
-                where svec :: SVec n a
-                      svec = unsafeCoerce $ go v' sV (FS i)
+toSparse :: forall a n . (KnownNat n, Eq a) => a -> Vec n a -> SVec n a
+toSparse sparseVal = go (enumFin nat)
+       where go :: Eq a => Vec p (Fin n) -> Vec p a -> SVec n a
+             go VN VN                                = (sparseVal, empty)
+             go (VC f fs) (VC v vs) | v == sparseVal = svec
+                                    | otherwise      = putS svec f v
+               where svec :: Eq a => SVec n a
+                     svec = go fs vs
